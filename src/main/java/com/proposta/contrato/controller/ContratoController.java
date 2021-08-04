4103089f251224ffcf8e6428c73d2eb3365abf12
Ricardo Mateus <ricardomateus23@hotmail.com>
2021-08-03 22:02:12 -0300

    add do metodo de atualizar

--- src/main/java/com/proposta/contrato/controller/ContratoController.java
+++ src/main/java/com/proposta/contrato/controller/ContratoController.java
@@ -2,11 +2,15 @@ package com.proposta.contrato.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,6 +41,17 @@ public class ContratoController {
 	public ResponseEntity<List<Contrato>> listarContratos(){
 		return ResponseEntity.ok().body(contratoRepository.findAll());
 
+	}
+	
+	@PutMapping
+	public ResponseEntity<Contrato> atualizar(@Valid @RequestBody Contrato usuario){
+		return contratoRepository.findById(usuario.getId()).map(record -> 
+						{
+							BeanUtils.copyProperties(usuario, record, "id");
+							contratoRepository.save(record);
+							return ResponseEntity.ok().body(record);
+						})
+		           .orElse(ResponseEntity.notFound().build());
 	}
 
 }
--- src/main/java/com/proposta/contrato/entity/Contrato.java
+++ src/main/java/com/proposta/contrato/entity/Contrato.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -21,12 +22,16 @@ public class Contrato {
 	@GeneratedValue
 	private Long id;
 	
+	@NotNull(message = "Codigo da proposta é obrigatório")
 	private Long codigoProposta;
+	@NotNull(message = "Codigo do cliente é obrigatório")
 	private Long codigoCliente;
 	
+	@NotNull(message = "Data é obrigatória")
 	@Temporal(TemporalType.DATE)
 	private Date dataContrato;
 	
+	@NotNull(message = "Valor de contrato é obrigatório")
 	private BigDecimal valorContrato;
 	
 	
